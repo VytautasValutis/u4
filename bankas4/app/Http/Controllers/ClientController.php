9be1dc61757839668be540d6a593fa8008a0ad7b
Vytautas Valutis <dnc908@gmail.com>
2023-04-10 13:39:02 +0300

    U4 validate + error msg

--- bankas4/app/Http/Controllers/ClientController.php
+++ bankas4/app/Http/Controllers/ClientController.php
@@ -4,9 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Models\client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
+    private function putRandCode() : string
+    {
+        $met = rand(1901,2007);
+        $men = rand(1, 12);
+        $nr = rand(1, 999);
+        if(in_array($men,[1,3,5,7,8,10,12])) {
+            $dien = rand(1, 31);
+        };
+        if(in_array($men,[4,6,9,11])) {
+            $dien = rand(1, 30);
+        };
+        if($men == 2) {
+            if($met % 4 === 0) {
+                $dien = rand(1, 29);
+            } else {
+                $dien = rand(1, 28);
+            }
+        }
+        if($met > 1999) {
+            $ak[] = rand(5, 6);
+        } else {
+            $ak[] = rand(3, 4);
+        }
+        $ak[] = floor(($met % 100) / 10);
+        $ak[] = $met % 10;
+        $ak[] = floor($men / 10);
+        $ak[] = $men % 10;
+        $ak[] = floor($dien / 10);
+        $ak[] = $dien % 10;
+        $ak[] = floor($nr / 100);
+        $ak[] = floor(($nr % 100) / 10);
+        $ak[] = $nr % 10;
+        $ks = $ak[0] + $ak[1] * 2 + $ak[2] * 3 +
+            $ak[3] * 4 + $ak[4] * 5 + $ak[5] * 6 +
+            $ak[6] * 7 + $ak[7] * 8 + $ak[8] * 9 + 
+            $ak[9];
+        $kss = $ks % 11;    
+        if($kss === 10) {
+            $ks = $ak[0] * 3 + $ak[1] * 4 + $ak[2] * 5 +
+                $ak[3] * 6 + $ak[4] * 7 + $ak[5] * 8 +
+                $ak[6] * 9 + $ak[7] + $ak[8] + $ak[9];
+            $kss = $ks % 11;
+            if($kss === 10) $kss = 0;
+        }        
+        $ak[] = $kss;
+    
+        return implode('', $ak);
+    }
+
+    private function accNr() : string
+    {
+        $sask_nr = 'LT3306660' . sprintf('%1$011d', time());
+        return $sask_nr;
+    }
+
+
     public function index()
     {
         $clients = Client::all()->sortBy('surname');
@@ -55,18 +112,32 @@ class ClientController extends Controller
     }
     public function create()
     {
+        session(['pid' => self::putRandCode()]);
+        session(['accNr' => self::AccNr()]);
         return view('clients.create');
     }
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'surname' => 'required|min:3',
+            // 'pid' => 'unique:App\Models\Client,pid',
+            'pid' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            $request->flash();
+            return redirect()
+                ->back()
+                ->withErrors($validator);
+        }
+
         $client = new Client;
         $client->name = $request->name;
         $client->surname = $request->surname;
-        // $client->pid = $request->pid;
-        $client->pid = '12345678901';
-        // $client->accNr = $request->accNr;
-        $client->accNr = 'LT123456789012345678';
+        $client->pid = $request->pid;
+        $client->accNr = $request->accNr;
         $client->value = 0;
         $client->save();
         return redirect()->route('clients-index');
@@ -76,12 +147,6 @@ class ClientController extends Controller
     {
         //
     }
-    // public function editA(client $client)
-    // {
-    //     return view('clients.addVal', [
-    //         'client' => $client
-    //     ]);
-    // }
     public function edit($oper, client $client)
     {
         if($oper == 'Add') return view('clients.addVal', ['client' => $client]);
